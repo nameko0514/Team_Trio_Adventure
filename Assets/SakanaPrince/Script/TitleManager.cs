1976667ed42665bbf4b48517af941374ca6a817f
さかなきんぐ <sakana2005@icloud.com>
2025-04-22 22:31:08 +0900

    TitleScene仮を自分のファイルの中に作った

--- Assets/SakanaPrince/Script/TitleManager.cs
+++ Assets/SakanaPrince/Script/TitleManager.cs
@@ -1,16 +1,38 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class TitleManager : MonoBehaviour
+namespace matumoto
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class TitleManager : MonoBehaviour
     {
-        
-    }
+        [Header("点滅させたいテキスト")]
+        [SerializeField] Text brinkText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("点滅間隔")]
+        [SerializeField] float brinkingInterval = 0;
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+             //if (Input.GetKey(KeyCode.Space){ SceneManager.LoadScene(""); }
+            StartCoroutine(BrinkText());
+        }
+
+        private void Update()
+        {
+           // if (Input.GetKey(KeyCode.Space){ SceneManager.LoadScene(""); }
+        }
+
+        private IEnumerator BrinkText()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(brinkingInterval);
+               brinkText.enabled = !brinkText.enabled;
+            }
+        }
     }
 }
