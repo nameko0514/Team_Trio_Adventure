4678da645b586ad02d9fb3510df863f1855f8790
カツオのけつ <taka.sara.4738.1221@gmail.com>
2025-04-23 23:12:58 +0900

    サブクラスを作って、プレイヤーが動けるようにしました

---
+++ Assets/katuonoketu/01_Script/Player/Player_Movement.cs
@@ -0,0 +1,20 @@
+using fujiiYuma;
+using UnityEngine;
+
+namespace Takato
+{
+    public class Player_Movement : PlayerController
+    {
+
+        protected override void Start()
+        {
+            base.Start();  //ベースクラスの初期化呼び出し
+        }
+
+        protected override void Update()
+        {
+            base.Update();  //ベースクラスのUpdate処理の呼び出し
+        }
+
+    }
+}
