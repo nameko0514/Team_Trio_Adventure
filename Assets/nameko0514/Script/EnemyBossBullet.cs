b4393c762132aeae48efcb980f66cea0efc28dfe
nameko0514 <yumakun09141016@gmail.com>
2025-05-04 16:51:39 +0900

    ボス作成中

---
+++ Assets/nameko0514/Script/EnemyBossBullet.cs
@@ -0,0 +1,48 @@
+using fujiiYuma;
+using UnityEngine;
+
+public class EnemyBossBullet : MonoBehaviour
+{
+    [Tooltip("弾の処理")]
+    [Header("---弾のスピード---")]
+    [SerializeField] private float speed = 15;
+    [Header("---弾のダメージ---")]
+    [SerializeField] private float damage = 1;
+    [Header("---弾のライフタイム---")]
+    [SerializeField] private float lifetime = 1.5f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        Destroy(gameObject, lifetime);
+        Debug.Log("弾消滅");
+
+        //Rigitbody2Dの取得    
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        //弾のスピードを設定
+        rb.linearVelocity = transform.right * speed;
+    }
+
+    //ダメージ処理
+    public float GetDamage()
+    {
+        return damage;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.TryGetComponent<PlayerController>(out var player))
+            {
+                const int LethalDamage = 1;
+                player.TakeDamage(LethalDamage);
+            }
+        }
+    }
+}
